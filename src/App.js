069bbc7ea58353133cbb9f2bc6d55f651104650f
Safal Mahat <safalmahat5@gmail.com>
2020-05-04 15:32:22 +0545

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ResultComponent from './components/ResultComponent';
+import KeypadComponent from "./components/KeypadComponent";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            result: ""
+        }
+    }
+
+    onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "AC"){
+            this.reset()
+        }
+
+        else if(button === "%"){
+          this.percent()
+        }
+        else if(button === "SR"){
+          this.squareRoot()
+        }
+        else if(button === "^"){
+          this.square()
+        }
+        else if(button === "e"){
+          this.exponential()
+        }
+        else if(button === "CE"){
+            this.backspace()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+
+    calculate = () => {
+        var checkResult = ''
+        if(this.state.result.includes('--')){
+            checkResult = this.state.result.replace('--','+')
+        }
+
+        else {
+            checkResult = this.state.result
+        }
+
+        try {
+            this.setState({
+                // eslint-disable-next-line
+                result: (eval(checkResult) || "" ) + ""
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+    };
+
+    percent = () => {
+          this.setState({
+          result:this.state.result /100
+        })
+    };
+
+    squareRoot = () => {
+        this.setState({
+            result: Math.sqrt(this.state.result)
+        })
+    };
+
+    square = () => {
+        this.setState({
+            result: Math.pow(this.state.result, 2)
+        })
+    };
+
+    exponential = () => {
+        this.setState({
+            result: Math.exp(this.state.result)
+        })
+    };
+
+
+    render() {
+        return (
+            <div>
+                <div className="calculator-body">
+                    <Header />
+                    <ResultComponent result={this.state.result}/>
+                    <KeypadComponent onClick={this.onClick}/>
+                    <Footer />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class ResultComponent extends Component {
+
+
+    render() {
+          return (
+            <div>
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="#">The Simple Calculator</a>
+                        </div>
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Home</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            )
+   }
+}
+
+
+export default ResultComponent;
